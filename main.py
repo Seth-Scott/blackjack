721da9cdf0ab0841c0bb70fff65bae820ccab46c
Seth Scott <s.scottvancouverwa@gmail.com>
2022-01-29 19:20:54 -0800

    add images directory for readme, print python version and remove initial rules comments for game

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
+import platform
 import art
 import random
+
+print(f" python version: {platform.python_version()}")
 
 def blackjack_math(sum_cards):
     running_total = 0
